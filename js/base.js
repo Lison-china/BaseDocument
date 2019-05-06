208bb0ba2b1102fc50587fb340d521a69ea2dfc9
liusong0518 <939232129@qq.com>
2019-05-06 10:07:11 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -12,3 +12,13 @@
         a.style.border = ".5px solid transparent", o.appendChild(a), n.appendChild(o), n.removeChild(o)
     }
 }(window, document)
+//手机端页面有弹出层时，不让页面滚动。
+function bodyScroll(event) {//禁止页面滚动
+    event.preventDefault();
+}
+//弹出层隐藏的时候调用
+document.body.removeEventListener('touchmove', bodyScroll, false);
+$("body").css({ "position": "initial", "height": "auto" });
+//弹出层显示的时候调用
+document.body.addEventListener('touchmove', bodyScroll, false);
+$('body').css({ 'position': 'fixed', "width": "100%" });
